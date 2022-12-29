a08d840cbb6f845267854e5a70a43a68a2a841e1
IS <stabel@google.com>
2022-12-29 15:30:52 +0100

    commit message

--- src/index.php
+++ src/index.php
@@ -2,6 +2,6 @@
 
 echo "Hello, World from Docker! <br>";
 echo "Hello ECS! <br>";
-echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
+echo '<img src="https://www.docker.com/wp-content/uploads/2022/03/horizontal-logo-monochromatic-white.png">';
 
 ?>
